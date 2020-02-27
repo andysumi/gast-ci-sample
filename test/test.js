1f9dc362cf7e7b10019d78e8b660c15c20143493
andysumi <andysumi@ruby.net.tokai-u.jp>
2020-02-27 15:19:19 +0900

    参考URLの追加

--- test/test.js
+++ test/test.js
@@ -1,5 +1,11 @@
 /* global GasTap, sayHello */
 
+/*
+  参考
+  - https://medium.com/effilab/how-to-perform-google-add-on-automated-unit-testing-and-publishing-with-circle-ci-part-2-636c7589350e
+  - https://qiita.com/jiroshin/items/dcc398285c652554e66a
+*/
+
 function gastTestRunner() { // eslint-disable-line no-unused-vars
   if ((typeof GasTap) === 'undefined') { // GasT Initialization. (only if not initialized yet.)
     eval(UrlFetchApp.fetch('https://raw.githubusercontent.com/zixia/gast/master/src/gas-tap-lib.js').getContentText());
